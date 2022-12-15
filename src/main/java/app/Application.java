e1d4f08d17351dfa38e3e35478d2090d9b6dbfb2
YanaNik239 <jananikiforovskaya@yandex.ru>
2022-12-15 10:29:40 +0300

    window icon ready

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,5 +1,7 @@
 package app;
 import io.github.humbleui.jwm.*;
+
+import java.io.File;
 import java.util.function.Consumer;
 
 /**
@@ -17,10 +19,14 @@ public class Application  implements Consumer<Event> {
     public Application() {
         window = App.makeWindow();
         window.setEventListener(this);
+        window.setTitle("Java 2D");
         window.setWindowSize(900, 900);
         window.setWindowPosition(100, 100);
+        switch (Platform.CURRENT) {
+            case WINDOWS -> window.setIcon(new File("src/main/resources/windows.ico"));
+            case MACOS -> window.setIcon(new File("src/main/resources/macos.icns"));
+        }
         window.setVisible(true);
-        window.setTitle("Java 2D");
     }
 
     /**
