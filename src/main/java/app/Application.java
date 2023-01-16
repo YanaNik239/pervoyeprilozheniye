72853758cfdc9a2e0658a610bbd0ced28184f4f2
YanaNik239 <jananikiforovskaya@yandex.ru>
2023-01-16 13:48:09 +0300

    4 panels

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -6,6 +6,10 @@ import io.github.humbleui.jwm.skija.EventFrameSkija;
 import io.github.humbleui.skija.Canvas;
 import io.github.humbleui.skija.Surface;
 import misc.CoordinateSystem2i;
+import panels.PanelControl;
+import panels.PanelHelp;
+import panels.PanelLog;
+import panels.PanelRendering;
 
 import java.io.File;
 import java.util.function.Consumer;
@@ -24,23 +28,27 @@ public class Application implements Consumer<Event> {
     /**
      * отступы панелей
      */
-    private static final int PANEL_PADDING = 5;
+    public static final int PANEL_PADDING = 5;
     /**
      * радиус скругления элементов
      */
     public static final int C_RAD_IN_PX = 4;
     /**
-     * Первый заголовок
+     * панель легенды
      */
-    private final Label label;
+    private final PanelHelp panelHelp;
     /**
-     * Первый заголовок
+     * панель курсора мыши
      */
-    private final Label label2;
+    private final PanelControl panelControl;
     /**
-     * Первый заголовок
+     * панель рисования
      */
-    private final Label label3;
+    private final PanelRendering panelRendering;
+    /**
+     * панель событий
+     */
+    private final PanelLog panelLog;
 
 
     /**
@@ -49,19 +57,26 @@ public class Application implements Consumer<Event> {
     public Application() {
         // создаём окно
         window = App.makeWindow();
-
-        // создаём первый заголовок
-        label = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
-                4, 4, 1, 1, 1, 1, "Привет, мир!", true, true);
-
-        // создаём второй заголовок
-        label2 = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
-                4, 4, 0, 3, 1, 1, "Второй заголовок", true, true);
-
-        // создаём третий заголовок
-        label3 = new Label(window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING,
-                4, 4, 2, 0, 1, 1, "Это тоже заголовок", true, true);
-
+        // создаём панель рисования
+        panelRendering = new PanelRendering(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 0, 0,
+                3, 2
+        );
+        // создаём панель управления
+        panelControl = new PanelControl(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 3, 0,
+                2, 2
+        );
+        // создаём панель лога
+        panelLog = new PanelLog(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 0, 2,
+                3, 1
+        );
+        // создаём панель помощи
+        panelHelp = new PanelHelp(
+                window, true, PANEL_BACKGROUND_COLOR, PANEL_PADDING, 5, 3, 3, 2,
+                2, 1
+        );
 
         // задаём обработчиком событий текущий объект
         window.setEventListener(this);
@@ -133,13 +148,11 @@ public class Application implements Consumer<Event> {
         canvas.save();
         // очищаем канвас
         canvas.clear(APP_BACKGROUND_COLOR);
-        // рисуем заголовок
-        label.paint(canvas, windowCS);
-        // рисуем второй заголовок
-        label2.paint(canvas, windowCS);
-        // рисуем третий заголовок
-        label3.paint(canvas, windowCS);
-        // восстанавливаем состояние канваса
+        // рисуем панели
+        panelRendering.paint(canvas, windowCS);
+        panelControl.paint(canvas, windowCS);
+        panelLog.paint(canvas, windowCS);
+        panelHelp.paint(canvas, windowCS);
         canvas.restore();
     }
 }
