e07610c13e64d9236df25e0a97ee58ecf561a4e9
YanaNik239 <jananikiforovskaya@yandex.ru>
2023-04-03 13:41:55 +0300

    scale works

--- src/main/java/app/Task.java
+++ src/main/java/app/Task.java
@@ -36,6 +36,10 @@ public class Task {
             и разность этих множеств""";
 
     /**
+     * коэффициент колёсика мыши
+     */
+    private static final float WHEEL_SENSITIVE = 0.001f;
+    /**
      * Вещественная система координат задачи
      */
     @Getter
@@ -284,5 +288,18 @@ public class Task {
      */
     public boolean isSolved() {
         return solved;
+    }
+    /**
+     * Масштабирование области просмотра задачи
+     *
+     * @param delta  прокрутка колеса
+     * @param center центр масштабирования
+     */
+    public void scale(float delta, Vector2i center) {
+        if (lastWindowCS == null) return;
+        // получаем координаты центра масштабирования в СК задачи
+        Vector2d realCenter = ownCS.getCoords(center, lastWindowCS);
+        // выполняем масштабирование
+        ownCS.scale(1 + delta * WHEEL_SENSITIVE, realCenter);
     }
 }
