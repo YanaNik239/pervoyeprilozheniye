f21b7443634983ee753c8804d338862e31560c3f
YanaNik239 <jananikiforovskaya@yandex.ru>
2023-04-17 13:25:56 +0300

    info panel

--- src/main/java/panels/PanelControl.java
+++ src/main/java/panels/PanelControl.java
@@ -3,6 +3,7 @@ package panels;
 import app.Point;
 import app.Task;
 import controls.*;
+import dialogs.PanelInfo;
 import io.github.humbleui.jwm.*;
 import io.github.humbleui.skija.Canvas;
 import misc.CoordinateSystem2i;
@@ -177,6 +178,7 @@ public class PanelControl extends GridPanel {
                 String s = "Задача решена\n" +
                         "Пересечений: " + PanelRendering.task.getCrossed().size() / 2 + "\n" +
                         "Отдельных точек: " + PanelRendering.task.getSingle().size();
+                PanelInfo.show(s + "\n\nНажмите Esc, чтобы вернуться");
                 PanelLog.success(s);
                 solve.text = "Сбросить";
             } else {
